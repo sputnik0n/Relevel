e2e342f175e71566ed6efd2b204a094874c49a25
sputnik0n <61326396+sputnik0n@users.noreply.github.com>
2022-02-03 00:34:08 +0530

    Add files via upload

---
+++ Relevel/Assignments/Level 1/Module 1/feb-1.js
@@ -0,0 +1,6 @@
+let num1 = prompt();
+let num2 = prompt();
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) + parseInt(num2)));
+console.log('The difference of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) - parseInt(num2)));
+console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) * parseInt(num2)));
+console.log('The quotient of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) / parseInt(num2)));
