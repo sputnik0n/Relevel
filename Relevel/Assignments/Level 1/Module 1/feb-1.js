5f05d1a762e70dec78bbea3ff0bcc6b2ed57b145
sputnik0n <61326396+sputnik0n@users.noreply.github.com>
2022-02-03 01:18:53 +0530

    Update feb-1.js

--- Relevel/Assignments/Level 1/Module 1/feb-1.js
+++ Relevel/Assignments/Level 1/Module 1/feb-1.js
@@ -1,6 +1,9 @@
-let num1 = prompt();
-let num2 = prompt();
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) + parseInt(num2)));
-console.log('The difference of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) - parseInt(num2)));
-console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) * parseInt(num2)));
-console.log('The quotient of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) / parseInt(num2)));
+let num1 = prompt('Enter the first number');
+let num2 = prompt('Enter the second number');
+let operation = prompt('Choose an operation: +, -, *, /');
+switch(operation) {
+    case '+': console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) + parseInt(num2))); break;
+    case '-': console.log('The difference of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) - parseInt(num2))); break;
+    case '*': console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) * parseInt(num2))); break;
+    case '/': console.log('The quotient of ' + num1 + ' and ' + num2 + ' is ' + (parseInt(num1) / parseInt(num2))); break;
+}
