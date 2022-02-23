e9068d3783bc8ca6881c01171be167331f549889
sputnik0n <61326396+sputnik0n@users.noreply.github.com>
2022-02-23 12:07:15 +0530

    Add files via upload

---
+++ Relevel/Assignments/Level 1/Module 1/feb-19-2.js
@@ -0,0 +1,26 @@
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (sortedArray[middle] === key) {
+            // found the key
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            // continue searching to the right
+            start = middle + 1;
+        } else {
+            // search searching to the left
+            end = middle - 1;
+        }
+    }
+	// key wasn't found
+    return -1;
+}
+
+let arr = [3,7,6,1,9,10];
+let sortedArray = arr.sort((a,b) => a-b);
+let key = 10;
+console.log(binarySearch(sortedArray, key));
